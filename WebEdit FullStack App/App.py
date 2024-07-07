fc4c499f007c76273e19c610a6c354668f1af9fc
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-07-06 20:50:58 -0600

    Update App.py

--- WebEdit FullStack App/App.py
+++ WebEdit FullStack App/App.py
@@ -1,18 +1,34 @@
-import flask_cors
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import json
-import requests
-import os
-import sys
-import WordPredictor from WordPredictor
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Optional
+from WordFinisher import WordFinisher
 
-app = Flask('App')
-CORS(app)
+app = FastAPI()
 
-@app.route('/api', methods=['GET'])
-def api():
-    return jsonify({'message': 'Hello World!'})
+origins = ["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class WordRequest(BaseModel):
+    word: str
+
+@app.get("/receive_word")
+async def receive_word(word: Optional[str] = None):
+    if not word:
+        raise HTTPException(status_code=400, detail="No word provided")
+    
+    word_finisher = WordFinisher()
+    finished_word = word_finisher.predict_words(word)
+    print(f"Finished word: {finished_word}")
+    
+    return {"finished_word": finished_word}
 
 if __name__ == '__main__':
-    pass
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5001)
